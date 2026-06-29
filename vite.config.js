50f4e5fe072dc83d7c06872aa6a8d0b9f58a7576
krcho <krcho@comes.co.kr>
2026-06-29 17:44:15 +0900

    file set

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, open: true },
+  base: "/lqa/",    
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
